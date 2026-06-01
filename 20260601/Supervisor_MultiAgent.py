99dadd2e2f4b6e2bac447a856a7793c3d410f6fe
skn29teacher <skn29.teacher@gmail.com>
2026-06-01 16:12:47 +0900

    7

--- 20260601/Supervisor_MultiAgent.py
+++ 20260601/Supervisor_MultiAgent.py
@@ -31,3 +31,41 @@
 #  Supervisor--> 오케스트레이터
 
 # %%
+import os
+import re
+from pathlib import Path
+import chromadb
+from chromadb.utils import embedding_functions
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph,START,END
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+# openai client
+client = OpenAI()
+# 임베딩 함수  허깅페이스 :This is a sentence-transformers model 찾아라.
+st_ef =  embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name='jhgan/ko-sroberta-multitask'
+)
+chroma_client = chromadb.PersistentClient()  #DB
+collection = chroma_client.get_or_create_collection(   # table
+    name = 'Supervisor_MultiAgent',
+    embedding_function=st_ef
+)
+
+if collection.count() == 0:
+    collection.add(
+        ids=[
+            "doc_langgraph",
+            "doc_react",
+            "doc_rag",
+            "doc_supervisor",
+        ],
+        documents=[
+            "LangGraph는 상태 기반 그래프로 역할별 노드를 연결한다.",
+            "ReAct는 생각과 행동을 번갈아 수행하며 중간 도구를 사용할 수 있다.",
+            "RAG는 벡터DB에서 찾은 근거를 답변 생성에 사용한다.",
+            "Supervisor는 여러 에이전트의 순서와 책임을 조율한다.",
+        ],
+    )
