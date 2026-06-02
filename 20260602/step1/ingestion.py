10e248c48656fb22a9882bf5500072665114619c
skn29teacher <skn29.teacher@gmail.com>
2026-06-02 16:13:43 +0900

    7

--- 20260602/step1/ingestion.py
+++ 20260602/step1/ingestion.py
@@ -56,6 +56,87 @@ class DocumentIngestion:
         print(f'총 {len(documents)}개 문서 로딩 완료')
         return documents
     
+    def chunk_documents(self, documents: List[Document])->List[Document]:
+        '''문서청킹'''
+        print(f'문서청킹시작 : size = {config.CHUNK_SIZE} overlap : {config.CHUNK_OVERLAP}')
+        text_spliter = (RecursiveCharacterTextSplitter(chunk_size=config.CHUNK_SIZE
+                                                      ,chunk_overlap=config.CHUNK_OVERLAP
+                                                      ,length_function=len,
+                                                      separators = ['\n\n','\n','. ','! ','? ',' ',""]
+                                                      ))
+        chunks = ( text_spliter.split_documents(documents))
+        for idx, chunk in enumerate(chunks):
+            chunk.metadata.update({
+                'chunk_id':idx,
+                'source_type':'internal',
+                'chunk_size':len(chunk.page_content)
+            })
+        print(f'총 {len(chunks)}개 청크 생성')
+        return chunk
+    def create_vectorstore(
+        self,
+        chunks: List[Document],
+    ) -> Chroma:
+        """
+        Chroma 벡터스토어 생성
+        """
+
+        print("\n💾 벡터스토어 생성 중...")
+
+        print(
+            f"  - 저장 위치: "
+            f"{self.persist_directory}"
+        )
+
+        print(
+            f"  - 컬렉션명: "
+            f"{config.CHROMA_COLLECTION_NAME}"
+        )
+
+        vectorstore = Chroma.from_documents(
+            documents=chunks,
+            embedding=self.embeddings,
+            collection_name=
+                config.CHROMA_COLLECTION_NAME,
+            persist_directory=
+                self.persist_directory,
+        )
+
+        print(
+            "✅ 벡터스토어 생성 완료"
+        )
+
+        return vectorstore
+
+    def run(self):
+        """
+        전체 파이프라인 실행
+        """
+        print("=" * 60)
+        print(
+            " 문서 수집 및 "
+            "임베딩 파이프라인 시작"
+        )
+        print("=" * 60)
+        documents = self.load_documents()
+        if not documents:
+            raise ValueError(
+                f"{self.doc_path} "
+                f"에서 문서를 찾을 수 없습니다."
+            )
+        chunks = self.chunk_documents(  documents  )
+        vectorstore = ( self.create_vectorstore( chunks ) )
+        print("\n" + "=" * 60)
+        print(" 파이프라인 완료")
+        print(            f"원본 문서 수: "       f"{len(documents)}"       )
+        print(            f"생성 청크 수: "            f"{len(chunks)}"        )
+
+        print(            f"저장 위치: "            f"{self.persist_directory}"        )
+
+        print("=" * 60)
+        return vectorstore
+
+
 if __name__ =='__main__'    :
     config.validate()
     temp = DocumentIngestion()
