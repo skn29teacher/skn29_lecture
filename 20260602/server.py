e04c54a54f3d18f5b70b3e6a7037ea5cbc245967
skn29teacher <skn29.teacher@gmail.com>
2026-06-02 10:02:35 +0900

    2

--- 20260602/server.py
+++ 20260602/server.py
@@ -1,5 +1,9 @@
 from fastmcp import FastMCP
-mcp = FastMCP(name='calculator')
+mcp = FastMCP(name='SimpleServer')
+
+@mcp.tool
+def get_greeting(name:str)->str:
+    return f'안녕하세요 {name}님! mcp서버에서 보내는 메세지 입니다.'
 
 @mcp.tool
 def multiply(a:float, b:float)->float:
