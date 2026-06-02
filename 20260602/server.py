f0095b63369c265e408740a25048c68361e153d0
skn29teacher <skn29.teacher@gmail.com>
2026-06-02 09:27:47 +0900

    1

---
+++ 20260602/server.py
@@ -0,0 +1,12 @@
+from fastmcp import FastMCP
+mcp = FastMCP(name='calculator')
+
+@mcp.tool
+def multiply(a:float, b:float)->float:
+    '''Multiplies two numbers together.'''
+    return a*b
+
+if __name__ == '__main__':
+    mcp.run()
+
+# 실행은 uv run server.py    
