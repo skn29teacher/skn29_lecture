14ca626b90283b5ecc09cf3c668af9f0d87c5e45
skn29teacher <skn29.teacher@gmail.com>
2026-06-02 15:20:04 +0900

    6

---
+++ 20260602/step1/config.py
@@ -0,0 +1,26 @@
+'''설정관리'''
+import os
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+class Config:
+    '''시스템설정'''
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    EMBEDDING_MODEL = os.getenv('EMBEDDING_MODEL')
+    LLM_MODEL = os.getenv("LLM_MODEL")
+    LLM_TEMPERATURE = os.getenv("LLM_TEMPERATURE")
+    RETRIEVAL_TOP_K = int(os.getenv('RETRIEVAL_TOP_K'))
+    CHUNK_SIZE = int(os.getenv('CHUNK_SIZE'))
+    CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP"))
+    DOCUMENTS_PATH = os.getenv('DOCUMENTS_PATH','./data/internal_docs')
+
+    @classmethod
+    def validate(cls):
+        '''필수 설정 검증'''
+        if not cls.OPENAI_API_KEY:
+            raise ValueError("OPENAI_API_KEY 설정되지 않았습니다.")
+        if not os.path.exists(cls.DOCUMENTS_PATH):
+            os.makedirs(cls.DOCUMENTS_PATH,exist_ok=True)
+            print(f'문서디렉터리 생성:{cls.DOCUMENTS_PATH}')
+
+config = Config()
