4cf150da0dd265b60a3d57e96b6a028d333b88e0
skn29teacher <skn29.teacher@gmail.com>
2026-06-19 10:10:13 +0900

    2

--- 20260619_javascript_es6/01_es6_basic/main_01.js
+++ 20260619_javascript_es6/01_es6_basic/main_01.js
@@ -4,7 +4,12 @@ document.addEventListener('DOMContentLoaded',()=>{
     const termThis = document.getElementById("term-this")
     const profileContainer = document.getElementById('profile-card-container')
 
-
+    document.getElementById('btn-var-scope')?.addEventListener('click',()=>{
+        if(true){
+            var escapedVar = "var는 함수 스코프 블록 외부로 유출됩니다."
+        }
+        termScope.textContent = `블록 내부에서 var로 선언한 변수 호출 성공:\n "${escapedVar}"\n\이처럼 var는 if나 for 블럭 밖에서도 살아남아 전역 오염을 일으킵니다. ` ;
+    });
 
     
 
