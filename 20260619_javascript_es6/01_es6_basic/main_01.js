88febb6f6556a4999dcf8a4da79394f83916ba68
skn29teacher <skn29.teacher@gmail.com>
2026-06-19 09:51:49 +0900

    1

--- 20260619_javascript_es6/01_es6_basic/main_01.js
+++ 20260619_javascript_es6/01_es6_basic/main_01.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    const termScope = document.getElementById("term-scope");
+    const termScope = document.getElementById("term-scope");    
     const termConst = document.getElementById("term-const")
     const termThis = document.getElementById("term-this")
     const profileContainer = document.getElementById('profile-card-container')
+
+
+
+    
+
 });
