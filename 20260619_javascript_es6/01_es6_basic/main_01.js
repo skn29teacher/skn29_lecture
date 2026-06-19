f274f71170ed6b82236a1f3403510a8f222d2145
skn29teacher <skn29.teacher@gmail.com>
2026-06-19 10:21:28 +0900

    2

--- 20260619_javascript_es6/01_es6_basic/main_01.js
+++ 20260619_javascript_es6/01_es6_basic/main_01.js
@@ -11,6 +11,27 @@ document.addEventListener('DOMContentLoaded',()=>{
         termScope.textContent = `블록 내부에서 var로 선언한 변수 호출 성공:\n "${escapedVar}"\n\이처럼 var는 if나 for 블럭 밖에서도 살아남아 전역 오염을 일으킵니다. ` ;
     });
 
+    document.getElementById('btn-let-scope')?.addEventListener('click',()=>{
+        let errorMessage = "";
+        // 블럭 스코프 테스트
+        try{
+            if(true){
+                let isolatedLet = "let은 블럭 내부에 격리됩니다.";
+            }
+            // 블럭 밖에서   isolatedLet 호출 시도
+            console.log(isolatedLet);
+        }catch(e){
+            errorMessage += `[블럭 외부 참조에러]let/const는 블럭 밖에서 읽수 없습니다.\n 오류메세지는 ${e.message}`;
+        }
+        // TDZ  선언 전 호출 테스트
+        try{
+            console.log(tdzVariable);
+            let tdzVariable = '나중에 선언된 let 변수'
+        }catch(e){
+            errorMessage +=`[참조 에러] let/const는 호이스팅은 되지만 초기화 전에 참조불가영역 TDZ에 묶여 에러를 발생\n 오류메세지는 ${e.message}`
+        }
+        termConst.textContent = errorMessage;
+    });
     
 
 });
