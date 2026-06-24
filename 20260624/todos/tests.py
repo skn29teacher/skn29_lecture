676ea31d42d5831cad871832d662f7e4ad2ffbd1
skn29teacher <skn29.teacher@gmail.com>
2026-06-24 10:53:51 +0900

    3

--- 20260624/todos/tests.py
+++ 20260624/todos/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TodoViewTest(TestCase):
+    def test_about(self):
+        response = self.client.post("/about/")
+        self.assertContains(response,"데이터가 제출되었습니다")
+    def test_root(self):
+        response = self.client.get("/about/")
+        self.assertContains(response,"classView")
