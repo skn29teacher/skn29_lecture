66204ad0871f288b34d695ece095c25e65462c9a
skn29teacher <skn29.teacher@gmail.com>
2026-06-26 11:05:26 +0900

    3

--- AI_APPLICATION_DJANGO_V3/todoboard/settings/production.py
+++ AI_APPLICATION_DJANGO_V3/todoboard/settings/production.py
@@ -11,3 +11,7 @@ ALLOWED_HOSTS=env.list('ALLOWED_HOSTS', default=['localhost','127.0.0.1'])
 DATABASES = {
     'default': env.db('DATABASE_URL')
 }
+
+#운영환경용 정적 파일 수집 경로 설정
+# collectstatic 실행 시 앱 내부 및 STATICFILES_DIRS 안의 파일들이 지정한 폴더로 모임
+STATIC_ROOT = BASE_DIR / 'staticfiles'
