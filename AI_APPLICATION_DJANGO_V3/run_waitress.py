1d26e6d38dc268305d8960fd6fa6c85f6c3a56a1
skn29teacher <skn29.teacher@gmail.com>
2026-06-26 12:10:32 +0900

    4

---
+++ AI_APPLICATION_DJANGO_V3/run_waitress.py
@@ -0,0 +1,8 @@
+from waitress import server
+from todoboard.wsgi import application
+
+if __name__ == '__main__':
+    # 윈도우 환경에서 로컬테스트를 위해서 루프백주소(127.0.0.1)와 포트 8000번에 할당
+    # 운영환경의 동시성 대응을위해서 스레드 풀 개수를 4개로 지정
+    print('Starting Waitress WSGI sever on http://127.0.0.1:8000')
+    server(application, host='127.0.0.1', port=8000, threads=4)
