df6baf036b1d23378ca5ca5c441e138fb4fea858
skn29teacher <skn29.teacher@gmail.com>
2026-06-29 11:20:13 +0900

    3

--- DJANGO_V4/01_auth_project_setup/django_project/accounts/views.py
+++ DJANGO_V4/01_auth_project_setup/django_project/accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import CustomerUserCreationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomerUserCreationForm
 
 def home(request):
     return render(request,'main.html')
@@ -18,4 +21,21 @@ def signup(request):
 # 가입완료 안내 페이지 
 def signup_success(request):
     return render(request,'accounts/signup_success.html')
+
+# 로그인
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form':form})
+
+# 로그아웃
+def logout_view(request):
+    # 현재 세션데이터를 데이터베이스에서 만료시키고 쿠키를 삭제
+    auth_logout(request)
+    return request('home')
 
