b310002aba053e7169561ed933baeb1bf8dffa0a
skn29teacher <skn29.teacher@gmail.com>
2026-06-29 10:14:02 +0900

    2

---
+++ DJANGO_V4/01_auth_project_setup/django_project/accounts/urls.py
@@ -0,0 +1,7 @@
+# http://127.0.0.1:8000/accounts/
+from django.urls import path
+from . import views
+urlpatterns = [    
+    path('signup/', views.signup, name='signup'),
+    path('signup/success/', views.signup_success, name='signup_success')
+]
--- DJANGO_V4/01_auth_project_setup/django_project/django_project/urls.py
+++ DJANGO_V4/01_auth_project_setup/django_project/django_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # account 이름으로 시작하면...
+    path('accounts/', include('accounts.urls'))
 ]
