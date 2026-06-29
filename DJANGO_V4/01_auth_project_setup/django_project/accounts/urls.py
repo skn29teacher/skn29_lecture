73aacb5fcc7717f4a3782dd9d3f5fa420d129a83
skn29teacher <skn29.teacher@gmail.com>
2026-06-29 11:34:46 +0900

    3

--- DJANGO_V4/01_auth_project_setup/django_project/accounts/urls.py
+++ DJANGO_V4/01_auth_project_setup/django_project/accounts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [        
     path('', views.home, name='home'),
+    path('accounts/login', views.auth_login, name='login'),
+    path('accounts/logout', views.auth_logout, name='logout'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('/accountssignup/success/', views.signup_success, name='signup_success')    
+    path('accounts/signup/success/', views.signup_success, name='signup_success')
 ]
