452692d2063ad38362b5200cb2c31af763ca8357
skn29teacher <skn29.teacher@gmail.com>
2026-06-29 09:24:04 +0900

    1

--- DJANGO_V4/01_auth_project_setup/django_project/accounts/models.py
+++ DJANGO_V4/01_auth_project_setup/django_project/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 class CustomerUser(AbstractUser):
     nickname = models.CharaField(max_length=50, blanck=True, verbose_name='닉네임')
